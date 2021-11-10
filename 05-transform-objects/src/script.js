d9ec53e9dcb71f3d77fb5817fc06689e08fa8915
Nathan Brachotte <nathan.brachotte@klarna.com>
2021-11-10 15:26:49 +0100

    done playing around with groups and objects

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,10 +10,63 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+const group = new THREE.Group()
+scene.add(group)
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+)
+cube2.position.set(1.5, 0, 0)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+)
+cube3.position.set(-1.5, 0, 0)
+group.add(cube3)
+
+group.position.y = 1
+group.rotation.y = Math.PI * 0.25
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 0.7
+// mesh.position.y = -0.7
+// mesh.position.z = 0.7
+
+// Alternative solution to the above
+// mesh.position.set(0.8, -0.6, 1)
+
+// Vector3.position has lots of useful methods
+// .length = distance between the center of the scene and our mesh position
+// console.log(mesh.position.length())
+
+// scene.add(mesh)
+
+// Set the vector's length to 1
+// mesh.position.normalize()
+
+// To see axes on the scene
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+
+// Scale
+// mesh.scale.set(0.5, 0.5, 1)
+
+// Rotate objects: rotation or quaternion
+
+// mesh.rotation.set(Math.PI, 1, 0)
+// To change the axis order of rotation
+// mesh.rotation.reorder('YXZ')
 
 /**
  * Sizes
@@ -28,6 +81,15 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 0.2
+camera.position.x = 0.2
+// .distanceTo = distance between the camera and the mesh position
+
+// To focus the camera on a specific position
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
+
 scene.add(camera)
 
 /**
